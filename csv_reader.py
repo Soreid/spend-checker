3ef65b9e8ce38173b694c87fb2ce676c300afdb7
Sam Daley <sdaley818@gmail.com>
2025-11-29 15:26:19 -0700

    Set private variables and getter methods.

--- csv_reader.py
+++ csv_reader.py
@@ -1,11 +1,11 @@
 class csv_data:
     def __init__(self, path: str, delimiter: str=","):
         self.path = path
-        data = self.read_file(delimiter)
-        self.headers: list[str] = data[0]
-        self.rows: list[list[str]] = data[1]
+        data = self.__read_file(delimiter)
+        self.__headers: list[str] = data[0]
+        self.__rows: list[list[str]] = data[1]
     
-    def read_file(self, delimiter: str) -> tuple[list[str], list[list[str]]]:
+    def __read_file(self, delimiter: str) -> tuple[list[str], list[list[str]]]:
         headers = []
         rows = []
         with open(self.path) as f:
@@ -14,18 +14,21 @@ class csv_data:
                 rows.append(line[:-1].split(delimiter))
         return (headers, rows)
 
-    def __repr__(self):
-        header_string = "Header:\n"
-        for header in self.headers:
-            header_string += header + ", "
-        header_string = header_string[:-2]
+    def get_headers(self):
+        return self.__headers
 
+    def get_rows(self):
+        return self.__rows
+
+    def __repr__(self) -> str:
+        header_string = f"Header:\n{self.__str_line(self.__headers)}"
         rows_string = "Rows:\n"
-        for row in self.rows:
-            for item in row:
-                rows_string += item + ", "
-            rows_string = rows_string[:-2]
-            rows_string += '\n'
-
+        for row in self.__rows:
+            rows_string += self.__str_line(row)
         return header_string + '\n' + rows_string
         
+    def __str_line(self, data: list) -> str:
+        output = ""
+        for item in data:
+            output += item + ", "
+        return output[:-2] + '\n'
